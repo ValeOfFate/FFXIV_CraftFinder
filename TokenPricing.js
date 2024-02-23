a5f3f3dfeec1a547bba7b30eb1079a93d639fbbd
Matthew Hocking <61280123+ValeOfFate@users.noreply.github.com>
2024-02-23 00:05:54 -0500

    Logic And Sorting Cleanup

--- TokenPricing.js
+++ TokenPricing.js
@@ -1,17 +1,30 @@
 // Const URL
 const apiUrl =     'https://universalis.app/api/v2//';
 const apiUrlTest = 'https://universalis.app/api/v2/worlds';
+const tableLabels = ['ID', 'Item Name', 'World', 'Retainer', 'Total Price', 'Quantity', 'Price/Unit', 'Import Amount', 'Cost/Import']
 
-var itemIDs = '39630, 39704';
+var itemIDs = '';
 var worldVal= 'North-America';
 var numListings = '5'
 var numEntries = '0'
 var hq = true;
-var dataSetUnsorted = {};
+var itemListings = {};
 var mapVals = {};
+var langCode = 'en';
 
 // 5 Second Update Button
 async  function Update() {
+
+    // Maps loading
+    const mapRequest = await loadMaps();
+    const parsedMaps = await mapRequest.json();
+    
+    // Loads IDs to call for listing info
+    loadIDs(parsedMaps);
+    
+    // console.log(parsedMaps)
+    //console.log(itemIDs);
+
 
     // Unsorted Data loading
     const unsortedData = await getData(
@@ -21,30 +34,74 @@ async  function Update() {
         + '&entries=' + numEntries
         + '&hq=' + hq);
     const parsedData = await unsortedData.json();
-    console.log(parsedData.items); 
+    // console.log(parsedData.items); 
     
-    // Maps loading
-    const mapRequest = await loadMaps();
-    const parsedMaps = await mapRequest.json();
-    console.log(parsedMaps)
     
     // Turns Data into a condensed unsorted JSON object
     generateJSON(parsedData.items);
     
     // Maps data
-    mapJSON(dataSetUnsorted, parsedMaps);
-    console.log(dataSetUnsorted);
+    mapJSON(itemListings, parsedMaps);
+    formatJSON(itemListings);
+    console.log(itemListings);
 
-    loadTable('Data-Table', ['total', 'itemID'], parsedData.items);
+    var sortedData = [];
+    $.each(itemListings, function(index, data) {
+        sortedData.push([index, [data]]);
+    })
+
+    console.log(sortedData[0]);
+
+    sortedData.sort(function (a, b) {
+
+        // a and b will be two instances of your object from your list
+    
+        // possible return values
+        var a1st = -1; // negative value means left item should appear first
+        var b1st =  1; // positive value means right item should appear first
+        var equal = 0; // zero means objects are equal
+    
+        // compare your object's property values and determine their order
+        if (b[1][0].Cost_PerImport < a[1][0].Cost_PerImport) {
+            return b1st;
+        }
+        else if (a[1][0].Cost_PerImport < b[1][0].Cost_PerImport) {
+            return a1st;
+        }
+        else {
+            return equal;
+        }
+    });
+
+    var testJSON = JSON.parse(JSON.stringify(sortedData));
+    console.log(testJSON);
+    console.log(sortedData[0]);
+
+
+    loadTable('Data-Table', ['Item_ID', 'Map_Name', 'World_Location','Retainer_Name', 'Item_Price', 'Item_Quan', 'Item_PPU', 'Map_Imports', 'Cost_PerImport'], testJSON);
 }
 
 function loadTable(tableID, fields, data) {
     //$('#' + tableId).empty(); //not really necessary
     var rows = '';
+    var row = '<tr>';
+    $.each(tableLabels, function(index, label) {
+        row += '<th>' + label + '' + '</th>'
+    })
+    row += '</tr>'
+    rows += row;
     $.each(data, function(index, item) {
         var row = '<tr>';
         $.each(fields, function(index, field) {
-            row += '<td>' + item[field+''] + '</td>';
+            if (field == 'Map_Imports') {
+                row += '<td>' + item[1][0].Item_Map.Imports+'' + '</td>';
+            }
+            else if (field == 'Map_Name') {
+                row += '<td>' + item[1][0].Item_Map[langCode]+'' + '</td>';
+            }
+            else {
+                row += '<td>' + item[1][0][field+''] + '</td>';
+            }
         });
         rows += row + '<tr>';
     });
@@ -69,8 +126,8 @@ async function loadMaps() {
 function generateJSON(data) {
     $.each(data, function(index, element) {
         $.each(element.listings, function(indexList, elementList) {
-            console.log(elementList);
-            dataSetUnsorted[elementList.listingID] = {'Item_ID': element.itemID,
+            // console.log(elementList);
+            itemListings[elementList.listingID] = {'Item_ID': element.itemID,
             'Item_Quan': elementList.quantity,
             'Item_Price': elementList.total,
             'Item_PPU': elementList.pricePerUnit,
@@ -85,5 +142,17 @@ function generateJSON(data) {
 function mapJSON(data, maps) {
     $.each(data, function(index, element) {
         element.Item_Map = maps[element.Item_ID];
+    });
+}
+
+function formatJSON(data) {
+    $.each(data, function(index, element) {
+        element.Cost_PerImport = (Math.floor(element.Item_Price / (element.Item_Quan * element.Item_Map.Imports)))
+    })
+}
+
+function loadIDs(maps) {
+    $.each(maps, function(index, element) {
+        itemIDs += ', ' + index; 
     });
 }
